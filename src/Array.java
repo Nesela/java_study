7595ea394d1858c366dbd8b25e2a2524bf74c6c1
Nesela <icarus_cat@naver.com>
2026-03-24 18:29:29 +0900

    배열기초

---
+++ src/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] score = new int[5]; //배열 score를 선언)참조변수 배열의 생성
+        score[3] = 100;
+
+        System.out.println("score[0]=" + score[0]);
+        System.out.println("score[1]=" + score[1]);
+        System.out.println("score[2]=" + score[2]);
+        System.out.println("score[3]=" + score[3]);
+        System.out.println("score[4]=" + score[4]);
+
+        int value = score[3];
+        System.out.println("value="+value);
+    }
+}
